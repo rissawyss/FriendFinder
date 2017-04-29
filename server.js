f8fafa1437b03ac7c832b87dd2ac12512215dff1
Marissa Wyss <mwyss1@gmail.com>
2017-04-29 02:55:03 -0700

    updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -184,6 +184,6 @@ app.get("/api/results", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
