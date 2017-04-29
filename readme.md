24beafb68c5f998c42228cb7596750bd9b355567
Marissa Wyss <mwyss1@gmail.com>
2017-04-29 02:25:23 -0700

    added hw13 files

---
+++ readme.md
@@ -0,0 +1,82 @@
+#HW Week 13 - {Express - Friend Finder}
+Homework week 13 for UCLA Coding BootCamp.
+
+"FriendFinder" is a compatibility-based full-stack site application that will take in entries from user surveys, then compare the answers with those from other users. 
+
+The app will then display the name and picture of the "friend" with the best overall match.
+
+## Routing
+Using Express to handle routing, users will be able to view the home page, friends list, survey and results.
+    - GET Route to /survey which should display the survey page.
+    - GET route that leads to home.html which displays the home page by default.
+    - GET route with the url /api/friends.
+    - GET route to view matched results.
+    - POST routes /api/friends to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+## Technologies Used
+	 -HTML
+   -Node.js
+	 -Node Package Module Express
+	 -Node Package Module body-parser
+	 -Node Package Module path
+
+
+## CODE EXPLANATION - Routes
+```
+// Default route to display home page
+app.get( "/", function( req, res ) {
+    res.sendFile( path.join( __dirname, "/app/public", "home.html" ));
+  });
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public", "survey.html"));
+});
+
+// Route to display All Friends - provides JSON
+app.get("/api/friends", function(req, res) {
+        res.json(friends);
+        return;
+});
+```
+
+
+## CODE EXPLANATION - Tallying Survey Results
+To determine the user's most compatible friend, the user's results were converted into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+
+Then, the user's scores were compared to those from other users, question by question.  The differences were added in order to calculate the total Difference.
+
+The closest match will be the user with the least amount of difference.
+
+Using the POST route to handle incoming surveys, I loop through the existing friends list to compare scores to the new entry's scores, then push the score results into an array of objects.
+
+Sample code:
+
+```
+// Tally Survey and create New Friends - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newFriend = req.body;
+  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
+  //Loop through friends list to compare scores to new entry's scores
+  //Push score results into an array of objects, scoreTallyObj
+  for (var i = 0; i < friends.length; i++) {
+    var indvScore = [];
+    for (var j = 0; j < 10; j++) {
+      var matchScore = 0;
+      var muppetScore = friends[i].scores[j];
+      var newFriendScore = newFriend.scores[j]; 
+      if (muppetScore > newFriendScore) {
+        matchScore = matchScore + (muppetScore - newFriendScore);
+      } else if (muppetScore === newFriendScore) {
+        matchScore = matchScore + 0;
+      } else {
+        matchScore = matchScore + (newFriendScore - muppetScore);
+      }
+    indvScore.push(matchScore);
+    scoreTallyObj[i] = indvScore;
+    }
+    console.log("SCORE: " + indvScore);
+   }
+
+```
---
+++ server.js
@@ -0,0 +1,189 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// =============================================================
+//Muppet Characters (DATA)
+var friends = [{
+  name:"Kermit",
+  photo: "https://pbs.twimg.com/profile_images/839900475205955585/FMzXSOkV.jpg",
+  scores:[
+      2,
+      4,
+      5,
+      5,
+      2,
+      4,
+      1,
+      5,
+      4,
+      3
+    ]
+},
+{
+  name:"Miss Piggy",
+  photo:"https://pbs.twimg.com/profile_images/662349173467975680/UxlUTQL0.jpg",
+  scores:[
+      5,
+      3,
+      2,
+      4,
+      1,
+      2,
+      5,
+      1,
+      2,
+      5
+    ]
+},
+{
+  name:"Fozzy the Bear",
+  photo:"https://pbs.twimg.com/profile_images/119415120/3-fozzie.jpg",
+  scores:[
+      1,
+      5,
+      5,
+      2,
+      5,
+      4,
+      1,
+      2,
+      4,
+      4
+    ]
+},
+{
+  name:"Gonzo",
+  photo:"https://pbs.twimg.com/media/CG09H1XUcAApusv.jpg",
+  scores:[
+      3,
+      5,
+      5,
+      2,
+      3,
+      3,
+      4,
+      3,
+      2,
+      3
+    ]
+},
+{
+  name:"Animal",
+  photo:"https://pbs.twimg.com/profile_images/2002307628/Captura_de_pantalla_2012-03-17_a_la_s__22.14.48.png",
+  scores:[
+      5,
+      5,
+      3,
+      5,
+      1,
+      3,
+      2,
+      1,
+      1,
+      5
+    ]
+}];
+
+// =============================================================
+// ROUTES
+
+// Default route to display home page
+app.get( "/", function( req, res ) {
+    res.sendFile( path.join( __dirname, "/app/public", "home.html" ));
+  });
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public", "survey.html"));
+});
+
+// Route to display All Friends - provides JSON
+app.get("/api/friends", function(req, res) {
+        res.json(friends);
+        return;
+});
+
+// Object to capture array of score comparisons to existing friends list
+var scoreTallyObj = {};
+var totalScore = [];
+var matched;
+
+// Tally Survey and create New Friends - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newFriend = req.body;
+  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
+  //Loop through friends list to compare scores to new entry's scores
+  //Push score results into an array of objects, scoreTallyObj
+  for (var i = 0; i < friends.length; i++) {
+  	var indvScore = [];
+  	for (var j = 0; j < 10; j++) {
+  		var matchScore = 0;
+  		var muppetScore = friends[i].scores[j];
+  		var newFriendScore = newFriend.scores[j]; 
+  		if (muppetScore > newFriendScore) {
+  			matchScore = matchScore + (muppetScore - newFriendScore);
+	  	} else if (muppetScore === newFriendScore) {
+  			matchScore = matchScore + 0;
+	  	} else {
+	  		matchScore = matchScore + (newFriendScore - muppetScore);
+	  	}
+  	indvScore.push(matchScore);
+  	scoreTallyObj[i] = indvScore;
+  	}
+  	console.log("SCORE: " + indvScore);
+   }
+   	//Take the scoreTallyobject and get the sum of each index. Push that into the totalScore array
+	for (var k = 0; k < friends.length; k++) {
+		var ranking = scoreTallyObj[k];
+		var sum = 0;
+		for (var i = 0; i < ranking.length; i++) {
+			sum = sum + ranking[i];
+		}
+		totalScore.push(sum);
+		console.log("SUM: " + sum);
+	}
+	//Use Math.min to capture the minimum number of the totalScore array.
+	//Use indexOf to capture the index of friends matched to the minimum number
+	var minimum = Math.min.apply(null, totalScore);
+	console.log("MINIMUM: " + minimum);
+	matched = totalScore.indexOf(minimum);
+	console.log("MATCHED INDEX: " + matched);
+	var friendMatched = friends[matched].name;
+	console.log("YOU ARE MATCHED TO: " + friendMatched);
+	var friendPic = friends[matched].photo;
+
+  // friends.push(newFriend);
+
+  // res.json(newFriend);
+});
+
+
+//Display results on api/results page
+app.get("/api/results", function(req, res) {
+  var html = "<h1> Your Closest Match </h1>";
+  html += "<h2> NAME: " + friends[matched].name + "</h2>";
+  html += "<img src=" + friends[matched].photo + ">";
+  res.send(html);
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
